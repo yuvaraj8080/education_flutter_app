f0b8ea246f7f76a117eb3d1ff9673e22572e6141
Yuvaraj Dekhane <148527838+yuvaraj8080@users.noreply.github.com>
2024-06-14 00:56:45 +0530

    successfully completed job detail screen job , post job feed both are completed

--- lib/features/authentication/screens/PhoneNumber/widgets/verificationNumber.dart
+++ lib/features/authentication/screens/PhoneNumber/widgets/verificationNumber.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_job_app/common/widgets_login/appBar/appbar.dart';
+import 'package:get/get.dart';
 
+import '../../../../../constants/colors.dart';
 import '../../../../../constants/image_string.dart';
 
 class VerifyNumberScreen extends StatelessWidget {
@@ -10,27 +12,45 @@ class VerifyNumberScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return  Scaffold(
         appBar: const TAppBar(showBackArrow: true),
-        body: Column(children: [
+        body: Padding(
+          padding: const EdgeInsets.only(left:20,right:20),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+            /// CHEMISPHERE PNG IMAGE
+            SizedBox(width:170,child: Image.asset(TImages.Chemisphere,)),
 
-          /// CHEMISPHERE PNG IMAGE
-          SizedBox(width:170,child: Image.asset(TImages.Chemisphere,)),
-
-          /// VERIFICATION PAGE TITLE HARE
-          const SizedBox(height:50),
-          Text("Type the verification code",
-              style: Theme.of(context).textTheme.headlineSmall,
-              textAlign: TextAlign.center),
-          // const SizedBox(height:10),
-          Text("That we have sent",
-              style: Theme.of(context).textTheme.headlineSmall,
-              textAlign: TextAlign.center),
-          const SizedBox(height:10),
-          // const SizedBox(height:10),
-          Text("Enter your verification code below",
-              style: Theme.of(context).textTheme.bodyMedium,
-              textAlign: TextAlign.center),
+            /// VERIFICATION PAGE TITLE HARE
+            const SizedBox(height:50),
+            Text("Type the verification code",
+                style: Theme.of(context).textTheme.headlineSmall,
+                textAlign: TextAlign.center),
+            Text("That we have sent",
+                style: Theme.of(context).textTheme.headlineSmall,
+                textAlign: TextAlign.center),
+            const SizedBox(height:5),
+            Text("Enter your verification code below",
+                style: Theme.of(context).textTheme.bodyMedium,
+                textAlign: TextAlign.center),
 
 
-        ]));
+            ///  VERIFY TEXT BUTTON
+                Padding(
+                  padding: const EdgeInsets.only(top:50,left:40,right:40),
+                  child: SizedBox(
+                    height: 55,
+                    width: double.infinity,
+                    child: ElevatedButton(
+                      style: ButtonStyle(
+                          backgroundColor:
+                          WidgetStateProperty.all<Color>(TColors.darkGrey)),
+                      onPressed: ()=> Get.to(()=> const VerifyNumberScreen()),
+                      child: const Text("GET OTP"),
+                    ),
+                  ),
+                )
+
+          ]),
+        ));
   }
 }
