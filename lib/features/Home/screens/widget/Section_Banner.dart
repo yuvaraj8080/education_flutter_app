1f609dbcd20a3fb504edb0185503645d9828fd15
Yuvaraj Dekhane <148527838+yuvaraj8080@users.noreply.github.com>
2024-07-06 13:24:01 +0530

    already user taken from the backend profile screen completed home app bar completed

--- lib/features/Home/screens/widget/Section_Banner.dart
+++ lib/features/Home/screens/widget/Section_Banner.dart
@@ -3,7 +3,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_job_app/features/Home/controller/Url_Launcher_Controller.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart';
-
 import '../../../../common/widgets_login/images/circular_container.dart';
 import '../../../../common/widgets_login/images/t_Rounded_image.dart';
 import '../../../../constants/colors.dart';
@@ -25,7 +24,6 @@ class TSectionBanners extends StatelessWidget {
 
       if(controller.banners.isEmpty){
         return const TShimmerEffect(width: double.infinity, height: double.infinity);
-        // return const Center(child: Text("No Data Found"));
       }
       else{
         return Column(
