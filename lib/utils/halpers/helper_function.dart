6f0c45fd70b081affb9a5f676562b0bc832e85e0
Yuvaraj Dekhane <148527838+yuvaraj8080@users.noreply.github.com>
2024-07-27 17:53:33 +0530

    already user taken from the backend profile screen completed home app bar completed

--- lib/utils/halpers/helper_function.dart
+++ lib/utils/halpers/helper_function.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:intl/intl.dart';
 
 class THelperFunction{
 
@@ -10,27 +9,5 @@ class THelperFunction{
   static bool isDarkMode(BuildContext context){
     return Theme.of(context).brightness == Brightness.dark;
   }
-
-  static void navigateToScreen(BuildContext context, Widget screen){
-    Navigator.push(context,MaterialPageRoute(builder: (_)=> screen));
-  }
-
-  static String truncateText(String text, int maxLength){
-    if(text.length <= maxLength){
-      return text;
-    }
-    else{
-      return "${text.substring(0,maxLength)}...";
-    }
-  }
-
-  static String getFormattedDate(DateTime data,{String format = "dd MMM yyyy"}){
-    return DateFormat(format).format(data);
-  }
-
-  static List<T> removeDuplicates<T>(List<T>list){
-    return list.toSet().toList();
-  }
-
 
 }
